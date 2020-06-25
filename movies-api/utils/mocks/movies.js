e66b7edc73e5e47ff0b54c1c495db90d80474634
Hector Flores <Hector_Flores@eogresources.com>
2020-06-25 12:09:40 -0700

    Test for endpoints

--- movies-api/utils/mocks/movies.js
+++ movies-api/utils/mocks/movies.js
@@ -139,6 +139,22 @@ const moviesMock = [
   },
 ];
 
+function filteredMoviesMocks(tag) {
+  return moviesMock.filter((movie) => movie.tags.includes(tag));
+}
+
+class MoviesServiceMock {
+  async getMovies() {
+    return Promise.resolve(moviesMock);
+  }
+
+  async createMovie() {
+    return Promise.resolve(moviesMock[0]);
+  }
+}
+
 module.exports = {
   moviesMock,
+  filteredMoviesMocks,
+  MoviesServiceMock,
 };
